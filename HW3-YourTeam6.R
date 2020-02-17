4b140d3c3b5c2e406eca5f23601a88d24fd6a2b4
mzaldiva2 <60248048+mzaldiva2@users.noreply.github.com>
2020-02-16 21:21:20 -0600

    Add files via upload

--- HW3-YourTeam6.R
+++ HW3-YourTeam6.R
@@ -11,6 +11,18 @@ library(MASS)
 n <- nrow(titanic)
 MVpercent <- round(MV/n, digits = 4)
 MV <- cbind(MV, MVpercent)
+
+#inconsistency for sex
+features <-data.frame( IV = matrix(0, nrow = 8, ncol = 1))
+features[2,1] <- sum((titanic$sex == "Male") | (titanic$sex == "Female"))
+features[7,1] <- sum(titanic$embarked == "Queenstown")
+MV <- cbind(MV, features)
+
+IVpercent <- round(features/n, digits = 4)
+MV <- cbind(MV, IVpercent)
+
+#--------------------------------------------------------------------------------------------
+#I'm still fixing ignore the rest of the code
 incon <- sapply(titanic, levels)
 incon
 #the inconsistencies in the feature of "embarked" should only be 2, because "Queenstown" and "Q" mean the same
